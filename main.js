59ce304994910a0f13d267a25cdb2329ee2d2f47
scott swenson <scottswenson33@gmail.com>
2024-10-23 12:50:20 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+'strict mode'
+
+var myAge = 42
+var earlyYears = 2
+var earlyYears = earlyYears * 10.5;
+var laterYears = myAge - 2;
+var laterYears = laterYears * 4;
+var myAgeInDogeYears = earlyYears + laterYears;
+// console.log(earlyYears );
+// console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+// console.log(myAgeInDogYears);
+
+var myName = "Scott Swenson".toLowerCase()
+// console.log(myName);
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
